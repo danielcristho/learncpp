09db88ca27beab2db738ed1191f654e824bee112
pepuhodaniel <69733783+pepuhodaniel@users.noreply.github.com>
2020-10-06 23:56:20 +0700

    Add files via upload
    commit 2

---
+++ hitung_luas_segitiga.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+
+using namespace std;
+
+
+int main(){
+    int A, T;
+    float luas;
+
+    scanf("d", &A);
+    scanf("d", &T);
+    luas=(float)A*T/2;
+    printf("s.2f\n", luas);
+   
+    return 0;
+}
