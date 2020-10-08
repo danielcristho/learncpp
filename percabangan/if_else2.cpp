67d93294c144384a080ff62d3d992fec58dc45b4
pepuhodaniel <69733783+pepuhodaniel@users.noreply.github.com>
2020-10-09 01:22:40 +0700

    many condition

---
+++ percabangan/if_else2.cpp
@@ -0,0 +1,26 @@
+# include<cstdio>
+
+int main() {
+    int x;
+    scanf("%d", &x);
+
+    if  (x > 0 ) {
+    printf("angka positif\n");
+    } else if (x == 0 ) {
+        printf(" nol\n");
+    } else   {
+        printf("negatif\n");
+    }
+    
+}      
+    
+    
+    
+        
+    
+    
+
+
+
+
+
