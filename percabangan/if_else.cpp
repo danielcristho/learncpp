f251d9cd7cdae8929ad6e24c5d6d67812781b713
pepuhodaniel <69733783+pepuhodaniel@users.noreply.github.com>
2020-10-09 01:18:53 +0700

    Add files via upload
    kondisi 2

---
+++ percabangan/if_else.cpp
@@ -0,0 +1,13 @@
+# include<cstdio>
+using namespace std;
+
+int main() {
+    int x;
+    scanf("%d", &x);
+
+    if (x > 0) {
+    printf("positif\n");
+    } else {
+        printf("non-positif\n");
+    }
+}
