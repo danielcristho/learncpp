230c6821a4c1eb34383d1bf073f2dc512f7836b5
pepuhodaniel <pepuhodaniel93.learn@gmail.com>
2021-06-02 18:41:02 +0900

    praktikum modul5

--- praktikum/jumlahanromeda2.c
+++ praktikum/jumlahanromeda2.c
@@ -50,9 +50,9 @@ int jumPulau(int M[][COL]) {
                      { 1, 0, 0, 1, 0},
                      { 0, 0, 0, 0, 0},
                      { 1, 0, 0, 1, 1} };*/
-      int M[COL][ROW];              
+    int M[COL][ROW];              
     int i, j;
-    
+
 
     for (i = 0; i<5;i++) {
         for (j = 0; j<5;j++) {
